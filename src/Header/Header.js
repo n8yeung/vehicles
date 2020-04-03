95db1d5e0e7364561bf7d6bdd2124179d33fb9d1
Nathan Yeung <nathan@nathanyeung.com>
2020-04-03 01:03:40 -0700

    Updated UI to improve experience

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Header.css';
+import logoMake from '../assets/images/logo-make.png';
 
 class Header extends Component {
 
   render() {
     return (
         <div className="header-container">
-          <h1>Honda Vehicles</h1>
+          <img alt="Honda" className="logo" src={logoMake} />
+          <h1 className="heading">Honda Vehicles</h1>
         </div>
     );
   }
